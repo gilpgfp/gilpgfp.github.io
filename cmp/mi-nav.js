0b17a4e764fa5579c70dcc8169bce3a04616bcc6
gilpgdmax <gilpgdamx@gmail.com>
2021-10-27 22:39:05 -0500

    Corrige navegación

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -8,143 +8,74 @@ export class MiNav extends HTMLElement {
         <li><p><a href="/index.html">Inicio</a></p></li>
         <li>
          <h3>Lecciones</h3>
-         <ol>
-          <li>
-           <details>
-            <summary>
-             <h4>
-              <a href="/1_fundamentos/index.html">Fundamentos</a>
-             </h4>
-            </summary>
-             <ol type="A">
-              <li>
-               <p><a href="/1_fundamentos/a_algoritmo.html">Algoritmo</a></p>
-              </li>
-              <li>
-               <p>
-                <a href="/1_fundamentos/b_hardware.html">Hardware</a>
-               </p>
-              </li>
-              <li>
-               <p><a href="/1_fundamentos/c_software.html">Software</a></p>
-              </li>
-              <li>
-               <p>
-                <a href="/1_fundamentos/d_lenguaje.html">Lenguaje de
-                 programación</a>
-               </p>
-              </li>
-              <li>
-               <p>
-                <a href="/1_fundamentos/e_compilador.html">Compilador</a>
-               </p>
-              </li>
-              <li>
-               <p><a href="/1_fundamentos/f_interprete.html">Intérprete</a></p>
-              </li>
-              <li>
-               <p>
-                <a href="/1_fundamentos/g_pseudocodigo.html">Pseudocódigo</a>
-               </p>
-              </li>
-              <li>
-               <p><a href="/1_fundamentos/h_resumen.html">Resumen</a></p>
-              </li>
-             </ol>
-            </details>
-           </li>
-           <li>
-             <details>
-               <summary>
-               <h4>
-                 <a href="/2_conceptos/index.html">Conceptos Básicos</a>
-               </h4>
-               </summary>
-               <ol type="A">
-                 <li>
-                   <p><a href="/2_conceptos/a_js.html">JavaScript</a></p>
-                 </li>
-                 <li>
-                   <p><a href="/2_conceptos/b_textos.html">Textos</a></p>
-                 </li>
-                 <li>
-                   <p>
-                     <a href="/2_conceptos/c_string.html">El tipo string</a>
-                   </p>
-                 </li>
-                 <li>
-                   <p><a href="/2_conceptos/d_write.html">document.write</a></p>
-                 </li>
-                 <li>
-                   <p><a href="/2_conceptos/e_orden.html">Orden de ejecución</a></p>
-                 </li>
-                 <li>
-                   <p><a href="/2_conceptos/f_resumen.html">Resumen</a></p>
-                 </li>
-               </ol>
-             </details>
-           </li>
-           <li>
-             <details>
-               <summary>
-                 <h4>
-                   <a href="/3_variables/index.html">Variables</a>
-                 </h4>
-               </summary>
-               <ol type="A">
-                 <li>
-                   <p>
-                     <a
-                       href="/3_variables/a_identificadores.html">Identificadores</a>
-                   </p>
-                 </li>
-                 <li>
-                   <p><a href="/3_variables/b_variables.html">Variables</a></p>
-                 </li>
-                 <li>
-                   <p><a href="/3_variables/c_constantes.html">Constantes</a></p>
-                 </li>
-                 <li>
-                   <p>
-                     <a href="/3_variables/d_duplicadas.html">Variables
-                       Duplicadas</a>
-                   </p>
-                 </li>
-                 <li>
-                   <p>
-                     <a href="/3_variables/e_no_encontradas.html">Variables no
-                       encontradas</a>
-                   </p>
-                 </li>
-                 <li>
-                   <p>
-                     <a href="/3_variables/f_prompt.html">La función prompt</a>
-                   </p>
-                 </li>
-                 <li>
-                   <p><a href="/3_variables/g_resumen.html">Resumen</a></p>
-                 </li>
-               </ol>
-             </details>
-           </li>
-           <li>
-             <details>
-               <summary>
-                 <h4>
-                   <a href="/4_numeros/index.html">Números</a>
-                 </h4>
-               </summary>
-               <ol type="A">
-                 <li>
-                   <p>
-                     <a
-                       href="/4_numeros/a_recta.html">Recta</a>
-                   </p>
-                 </li>
-               </ol>
-             </details>
-           </li>
-         </ol>
+         <details>
+          <summary>
+           <h4>
+            <a href="/1_fundamentos/index.html"><span>1.</span>
+             <span>Fundamentos</span></a>
+           </h4>
+          </summary>
+          <div>
+           <a href="/1_fundamentos/a_algoritmo.html">A. Algoritmo</a>
+          </div>
+          <div>
+           <a href="/1_fundamentos/b_hardware.html">B. Hardware</a>
+          </div>
+          <div>
+           <a href="/1_fundamentos/c_software.html">C. Software</a>
+          </div>
+          <div>
+           <a href="/1_fundamentos/d_lenguaje.html"><span>D.</span>
+           <span>Lenguaje de programación</span></a>
+          </div>
+          <div>
+           <a href="/1_fundamentos/e_compilador.html">E. Compilador</a>
+          </div>
+          <div>
+           <a href="/1_fundamentos/f_interprete.html">F. Intérprete</a>
+          </div>
+          <div>
+           <a href="/1_fundamentos/g_pseudocodigo.html">G. Pseudocódigo</a>
+          </div>
+          <div><a href="/1_fundamentos/h_resumen.html">H. Resumen</a></div>
+         </details>
+         <details>
+          <summary>
+           <h4><a href="/2_conceptos/index.html"><span>2.</span>
+           <span>Conceptos Básicos</span></a></h4>
+          </summary>
+          <div><a href="/2_conceptos/a_js.html">JavaScript</a></div>
+          <div><a href="/2_conceptos/b_textos.html">Textos</a></div>
+          <div><a href="/2_conceptos/c_string.html">El tipo string</a></div>
+          <div><a href="/2_conceptos/d_write.html">document.write</a></div>
+          <div><a href="/2_conceptos/e_orden.html">Orden de ejecución</a></div>
+          <div><a href="/2_conceptos/f_resumen.html">Resumen</a></div>
+         </details>
+         <details>
+          <summary>
+           <h4><a href="/3_variables/index.html">3. Variables</a></h4>
+          </summary>
+          <div>
+           <a href="/3_variables/a_identificadores.html">Identificadores</a>
+          </div>
+          <div><a href="/3_variables/b_variables.html">Variables</a></div>
+          <div><a href="/3_variables/c_constantes.html">Constantes</a></div>
+          <div>
+           <a href="/3_variables/d_duplicadas.html">Variables Duplicadas</a>
+          </div>
+          <div>
+            <a href="/3_variables/e_no_encontradas.html">Variables no
+             encontradas</a>
+          </div>
+          <div><a href="/3_variables/f_prompt.html">La función prompt</a></div>
+          <div><a href="/3_variables/g_resumen.html">Resumen</a></div>
+         </details>
+         <details>
+          <summary>
+           <h4><a href="/4_numeros/index.html">4. Números</a></h4>
+          </summary>
+          <div><a href="/4_numeros/a_recta.html">Recta</a></div>
+         </details>
         </li>
        </ul>`;
   }
