a315e4768cd9126b3973ffa203ec82eacfe4b71b
gilpgdmax <gilpgdamx@gmail.com>
2021-10-27 20:30:09 -0500

    Simplificación de estilos y detalles

--- cmp/gilpgfp-index.js
+++ cmp/gilpgfp-index.js
@@ -7,7 +7,7 @@ export class GilPGFPIndex extends Diapositiva {
   super.connectedCallback();
   this.innerHTML = /* html */
    `<div class="lectura">
-    <h2>por Gilberto Pacheco Gallegos</h2>
+    <address><p>por Gilberto Pacheco Gallegos</p></address>
     <p class="noPrint">
      <a href="print.html" target="_blank">Versión para Imprimir.</a>
     </p>
@@ -18,8 +18,10 @@ export class GilPGFPIndex extends Diapositiva {
      para aprender más sobre programación en JavaScript.
     </p>
     <instrucciones-nav></instrucciones-nav>
-    <h2>Contenido</h2>
-    <mi-nav></mi-nav>
+    <section>
+     <h1>Contenido</h1>
+     <mi-nav></mi-nav>
+    </section>
    </div>`
  }
 }
